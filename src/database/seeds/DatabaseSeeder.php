baeac935434513080ead5aa5e5d9d83b6380632b
watanabe-miyuki <watanabemiyuki@keio.jp>
2022-06-21 10:03:34 +0900

    webapp test 完了

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RecordTableSeeder::class,
             UserTableSeeder::class,
-            DayTableSeeder::class,
         ]);
     }
 }
